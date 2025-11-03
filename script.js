885861f1f0e04ed8f50ed12e047d0563a8d9891f
Mech-Kal <kalpakkamble02@gmail.com>
2025-11-04 01:16:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function firstChar(text) {
-  // Trim the leading and trailing spaces
+  // Trim leading and trailing spaces
   text = text.trim();
-  
-  // If string is empty after trimming, return empty string
+
+  // If the string is empty after trimming, return an empty string
   if (text === "") return "";
 
-  // Return the first character
+  // Return the first non-space character
   return text[0];
 }
 
 // Do not change the code below
-// Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
